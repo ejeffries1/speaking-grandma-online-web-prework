933ef38e4e5441d61ae5367216c23bca218e76c0
Evan Jeffries <ejeffries14@gmail.com>
2019-05-31 23:47:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
+def speak_to_grandma(phrase)
   
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
+if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak_to_grandma == .upcase
   puts "NO, NOT SINCE 1938!"
